07fdd99143187b1d89e7d8f17b0de4b651cea0a3
andries bron <abron@andries.local>
2017-03-05 18:35:33 +0100

    Test

--- client/python/radenium.py
+++ client/python/radenium.py
@@ -20,7 +20,6 @@ This file is part of Radenium.
         
 
 if __name__ == "__main__":
-    print "Hi this is my first line of code for this awesome project lets test and commit!"
-    print "Trying again"
-    print "and againkjhlkjhhj"
+    #! Which it always is:
+    print "Running Radenium!"
     
