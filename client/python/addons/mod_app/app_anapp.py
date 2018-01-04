23ef7f642a0ffd947cac83845e1ba24467519dbc
andries bron <abron@andries.local>
2018-01-04 23:03:55 +0100

    Added documentation

--- client/python/addons/mod_app/app_anapp.py
+++ client/python/addons/mod_app/app_anapp.py
@@ -4,25 +4,38 @@
 
 import sys
 
-sys.path.append('../mod_app')
+
 try:
+    sys.path.append('../mod_app')
     import mod_app
 except:
     pass
 
 
-
-
 class app_anapp(mod_app.mod_app):
+    """ Example class that implements the mod_app usage
+    Mod_app is the default threaded application class used by radenium.py to integrate all applications.
+    """
     def __init__(self, outqueue=None):
+        """ Constructor
+            An queue can be applied to which the application will send matter that is interesting for outer space.
+        """
         #! Registering app worker, called in run thread and the queue for anything interesting to be send to outer space.
         super().__init__(self.worker, outqueue)
     
+    
     def worker(self, event=None):
+        """ Called by super().run() function
+        This function gets called periodically by the run function of the mod_app class.
+        """
         pass
 
+
     def stopApplication(self):
+        """ Called by super().stop() function
+        """
         pass
+
 
 if __name__ == "__main__":
     app=app_eventhandler()
