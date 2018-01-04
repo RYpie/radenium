d92d127343d4d8f0ad595119387529c570ab3c2a
andries bron <abron@andries.local>
2018-01-04 22:09:35 +0100

    updated the example app.

--- client/python/addons/mod_app/app_anapp.py
+++ client/python/addons/mod_app/app_anapp.py
@@ -15,16 +15,16 @@ except:
 
 class app_anapp(mod_app.mod_app):
     def __init__(self, outqueue=None):
+        #! Registering app worker, called in run thread and the queue for anything interesting to be send to outer space.
         super().__init__(self.worker, outqueue)
     
     def worker(self, event=None):
-        print("in worker")
-        print(event)
-        if self.inqueue.empty():
-            self.runAlways = False
+        pass
 
+    def stopApplication(self):
+        pass
 
 if __name__ == "__main__":
     app=app_eventhandler()
-    app.pushEvent(['hi'])
+    app.pushEvent(['this_is_an_event'])
     app.run()
